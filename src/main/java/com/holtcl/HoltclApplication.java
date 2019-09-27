38e6a1f4a77238bf6e7cbe5bc806d02e96063526
Administrator <Administrator@DESKTOP-099UQRG>
2019-09-26 21:12:19 -0400

    Initial Commit of Spring Boot Project

---
+++ src/main/java/com/holtcl/HoltclApplication.java
@@ -0,0 +1,13 @@
+package com.holtcl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HoltclApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HoltclApplication.class, args);
+	}
+
+}
